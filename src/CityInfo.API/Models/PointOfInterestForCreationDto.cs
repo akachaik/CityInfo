f972e00ef36c9b215c759da3552fb8f04110727d
Your Name <you@example.com>
2016-12-25 22:49:32 +0700

    Validate input

--- src/CityInfo.API/Models/PointOfInterestForCreationDto.cs
+++ src/CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -1,9 +1,25 @@
-﻿namespace CityInfo.API.Models
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
 {
-    public class PointOfInterestForCreationDto
+    public class PointOfInterestForCreationDto : IValidatableObject
     {
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+
+        [MaxLength(200)]
         public string Description { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Name == Description)
+            {
+                yield return
+                    new ValidationResult("Name and descrition should be different", new[] {"Name", "Description"});
+            }
+        }
     }
 }
