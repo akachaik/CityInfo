69094cb956739fcb6d67ab6b5c32dd6d1c4035b4
Your Name <you@example.com>
2016-12-25 22:13:06 +0700

    Extract dto

--- src/CityInfo.API/Models/CityDto.cs
+++ src/CityInfo.API/Models/CityDto.cs
@@ -19,12 +19,4 @@ namespace CityInfo.API.Models
 
         public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } = new List<PointOfInterestDto>();
     }
-
-    public class PointOfInterestDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-
-    }
 }
