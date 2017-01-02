eec7306111e47fc9476c4729c9d21d12b89f8cf6
Your Name <you@example.com>
2017-01-02 14:34:15 +0700

    Add configuration files

--- src/CityInfo.API/Services/CloudMailService.cs
+++ src/CityInfo.API/Services/CloudMailService.cs
@@ -9,8 +9,8 @@ namespace CityInfo.API.Services
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "admin@mail.com";
-        private string _mailFrom = "noreply@mail.com";
+        private string _mailTo = Startup.Configuration["mailSettings:mailToAddress"];
+        private string _mailFrom = Startup.Configuration["mailSettings:mailFromAddress"];
 
         public void SendMail(string subject, string message)
         {
--- src/CityInfo.API/Startup.cs
+++ src/CityInfo.API/Startup.cs
@@ -11,11 +11,23 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
 using NLog.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace CityInfo.API
 {
     public class Startup
     {
+        public static IConfiguration Configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
