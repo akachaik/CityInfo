afb0113a00be4f8cf0f1139f3d70243bc47966d9
Your Name <you@example.com>
2017-01-02 23:51:06 +0700

    Add repository

---
+++ src/CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CityInfo.API.Entities;
+using CityInfo.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        private readonly CityInfoContext _context;
+
+        public CityInfoRepository(CityInfoContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<City> GetCities()
+        {
+            return _context.Cities.OrderBy(c => c.Name).ToList();
+        }
+
+        public City GetCity(int cityId, bool includePointsOfInterest)
+        {
+            return includePointsOfInterest ? 
+                _context.Cities.Include(c => c.PointsOfInterest).FirstOrDefault(c => c.Id == cityId) : 
+                _context.Cities.FirstOrDefault(c => c.Id == cityId);
+        }
+
+        public IEnumerable<PointOfInterest> GetPointsOfInterest(int cityId)
+        {
+            return _context.PointsOfInterest.Where(p => p.CityId == cityId).ToList();
+        }
+
+        public PointOfInterest GetPointOfInterest(int cityId, int pointOfInterestId)
+        {
+            return
+                _context.PointsOfInterest.FirstOrDefault(p => p.Id == pointOfInterestId && p.CityId == cityId);
+        }
+    }
+}
---
+++ src/CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        IEnumerable<City> GetCities();
+        City GetCity(int cityId, bool includePointsOfInterest);
+        IEnumerable<PointOfInterest> GetPointsOfInterest(int cityId);
+        PointOfInterest GetPointOfInterest(int cityId, int pointOfInterestId);
+    }
+}
