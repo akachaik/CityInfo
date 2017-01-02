e3db71de3253bd4ea3316f73289c6704f68c5f92
Your Name <you@example.com>
2017-01-02 15:29:52 +0700

    Creating a DbContext

---
+++ src/CityInfo.API/Controllers/DummyController.cs
@@ -0,0 +1,22 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/dummy")]
+    public class DummyController : Controller
+    {
+        private readonly CityInfoContext _ctx;
+
+        public DummyController(CityInfoContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/CityInfo.API/Models/CityInfoContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CityInfo.API.Entities;
+
+namespace CityInfo.API.Models
+{
+    public sealed class CityInfoContext : DbContext
+    {
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<PointOfInterest> PointsOfInterest { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("connestionString");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
